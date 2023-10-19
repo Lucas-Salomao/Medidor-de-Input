6d142b2a238899f3da829d133ca8e1e6cfc5ac56
Lucas-Salomao <lucastadeusalomao@gmail.com>
2023-10-19 18:42:37 -0300

    primeiro teste com menu LCD

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,23 @@
 /*
- Basic Menu
+Copyright (c) Lucas Salomao <lucastadeusalomao@gmail.com>
 
- https://lcdmenu.forntoh.dev/examples/basic
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
 
 */
 
@@ -9,6 +25,7 @@
 
 #define LCD_ROWS 2
 #define LCD_COLS 16
+#define LCD_ADDR 0x27
 
 // Configure keyboard keys (ASCII)
 #define UP 56        // NUMPAD 8
@@ -22,11 +39,9 @@
 
 // Initialize the main menu items
 MAIN_MENU(
-    ITEM_BASIC("Start service"),
-    ITEM_BASIC("Connect to WiFi"),
-    ITEM_BASIC("Settings"),
-    ITEM_BASIC("Blink SOS"),
-    ITEM_BASIC("Blink random")
+    ITEM_BASIC("Nº Voltas"),
+    ITEM_BASIC("Calibração tensao"),
+    ITEM_BASIC("Media de tempo")
 );
 // Construct the LcdMenu
 LcdMenu menu(LCD_ROWS, LCD_COLS);
@@ -34,7 +49,7 @@ LcdMenu menu(LCD_ROWS, LCD_COLS);
 void setup() {
     Serial.begin(115200);
     // Initialize LcdMenu with the menu items
-    menu.setupLcdWithMenu(0x27, mainMenu);
+    menu.setupLcdWithMenu(LCD_ADDR, mainMenu);
 }
 
 void loop() {
