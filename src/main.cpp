6b9ee475ce77f3810f46ff5e0a8c2e195e100712
Lucas-Salomao <lucastadeusalomao@gmail.com>
2023-10-28 09:54:57 -0300

    adicionado lógica para encoder interar com LCD

--- src/main.cpp
+++ src/main.cpp
@@ -29,12 +29,17 @@ THE SOFTWARE.
 #include <ArduinoJson.h>
 #include <StreamUtils.h>
 #include <EEPROM.h>
+#include <RotaryEncoder.h>
+#include <OneButton.h>
 
 #define LCD_ROWS 2
 #define LCD_COLS 16
 #define LCD_ADDR 0x27
 #define CHARSET_SIZE 11
 #define CONFIG_ADDR 0x00
+#define swEncoder 7
+#define ckEncoder A2
+#define dtEncoder A3
 
 // Configure keyboard keys (ASCII)
 #define UP 56       // NUMPAD 8
@@ -45,6 +50,14 @@ THE SOFTWARE.
 #define BACK 55     // NUMPAD 7
 #define BACKSPACE 8 // BACKSPACE
 #define CLEAR 46    // NUMPAD .
+
+// Pinos de ligacao do encoder
+RotaryEncoder encoder(ckEncoder, dtEncoder);
+// Variavel para o botao do encoder
+int valor = 0;
+int newPos = 0;
+// Setup a new OneButton on pin A1.
+OneButton button1(A1, true);
 
 extern MenuItem *settingsMenu[];
 
@@ -100,7 +113,8 @@ void load_configuration(void)
 
   EepromStream eepromStream(CONFIG_ADDR, sizeof(doc));
   DeserializationError error = deserializeJson(doc, eepromStream);
-  for (JsonPair pair : doc.as<JsonObject>()) {
+  for (JsonPair pair : doc.as<JsonObject>())
+  {
     Serial.print(pair.key().c_str());
     Serial.print(" = ");
     Serial.println(pair.value().as<String>().c_str());
@@ -135,14 +149,7 @@ void save_configuration()
   serializeJson(doc, eepromStream);
 }
 
-void setup()
-{
-  Serial.begin(115200);
-  init_LCD();
-  load_configuration();
-}
-
-void loop()
+void monitora_teclado()
 {
   if (!Serial.available())
     return;
@@ -185,6 +192,106 @@ void loop()
   default:
     break;
   }
+}
+
+void read_encoder()
+{
+  // //Verifica se o botao do encoder foi pressionado
+  // valor = digitalRead(swEncoder);
+  // if (valor != 1)
+  // {
+  //   Serial.println("Botao pressionado");
+  //   while (digitalRead(swEncoder) == 0)
+  //     delay(10);
+  // }
+  // //Le as informacoes do encoder
+  // static int pos = 0;
+  encoder.tick();
+  // int newPos = encoder.getPosition();
+  // //Se a posicao foi alterada, mostra o valor
+  // //no Serial Monitor
+  // if (pos != newPos) {
+  //   Serial.print(newPos);
+  //   Serial.println();
+  //   pos = newPos;
+  // }
+
+  if (encoder.getDirection() == RotaryEncoder::Direction::CLOCKWISE)
+  {
+    if (menu.isInEditMode()) // Update the position only in edit mode
+      charsetPosition =
+          constrain(charsetPosition - 1, 0, CHARSET_SIZE);
+    menu.drawChar(charset[charsetPosition]); // Works only in edit mode
+    menu.down();
+  }
+  if (encoder.getDirection() == RotaryEncoder::Direction::COUNTERCLOCKWISE)
+  {
+    if (menu.isInEditMode()) // Update the position only in edit mode
+      charsetPosition = (charsetPosition + 1) % CHARSET_SIZE;
+    menu.drawChar(charset[charsetPosition]); // Works only in edit mode
+    menu.up();
+  }
+  // if (digitalRead(swEncoder) == LOW)
+  // {
+  //   while (digitalRead(swEncoder) == LOW)
+  //     delay(10);
+  //   menu.type(charset[charsetPosition]); // Works only in edit mode
+  //   menu.enter();
+  // }
+}
+
+// ----- button 1 callback functions
+
+// This function will be called when the button1 was pressed 1 time (and no 2. button press followed).
+void click1()
+{
+  menu.type(charset[charsetPosition]); // Works only in edit mode
+  menu.enter();
+} // click1
+
+// This function will be called when the button1 was pressed 2 times in a short timeframe.
+void doubleclick1()
+{
+  menu.back();
+} // doubleclick1
+
+// This function will be called once, when the button1 is pressed for a long time.
+void longPressStart1()
+{
+  menu.backspace();
+} // longPressStart1
+
+// This function will be called often, while the button1 is pressed for a long time.
+void longPress1()
+{
+  Serial.println("Button 1 longPress...");
+} // longPress1
+
+// This function will be called once, when the button1 is released after beeing pressed for a long time.
+void longPressStop1()
+{
+  Serial.println("Button 1 longPress stop");
+} // longPressStop1
+
+void setup()
+{
+  Serial.begin(115200);
+  init_LCD();
+  load_configuration();
+  pinMode(swEncoder, INPUT);
+
+  // link the button 1 functions.
+  button1.attachClick(click1);
+  button1.attachDoubleClick(doubleclick1);
+  button1.attachLongPressStart(longPressStart1);
+  button1.attachLongPressStop(longPressStop1);
+  button1.attachDuringLongPress(longPress1);
+}
+
+void loop()
+{
+  monitora_teclado();
+  read_encoder();
 }
 
 void menu_back(void)
