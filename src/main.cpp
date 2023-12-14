685a6da9176fec0a5743ed25fcfd945f502b088b
Lucas-Salomao <lucastadeusalomao@gmail.com>
2023-12-13 21:24:23 -0300

    update

--- src/main.cpp
+++ src/main.cpp
@@ -243,12 +243,12 @@ void setup()
   last_time = 0;
   elapsed_time = 0;
 
-  //button.begin(BUTTON_PIN);
-  //button.setClickHandler(handler);
+  button.begin(BUTTON_PIN);
+  button.setClickHandler(handler);
   // button.setLongClickHandler(handler);       // this will only be called upon release
   //button.setLongClickDetectedHandler(handler); // this will only be called upon detection
-  //button.setDoubleClickHandler(handler);
-  //button.setTripleClickHandler(handler);
+  button.setDoubleClickHandler(handler);
+  button.setTripleClickHandler(handler);
 
   load_configuration();
   setupPWM16(pwm_resolution);
@@ -262,7 +262,7 @@ void loop()
 {
   //encoder->tick(); // just call tick() to check the state.
   read_encoder();
-  //button.loop();
+  button.loop();
   //test_dac();
   //test_pwm();
 }
